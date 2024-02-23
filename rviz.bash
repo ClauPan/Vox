3e9e74c5bb3774bb0a1a1f6e619351010434c907
Lokplart <lokplart@gmail.com>
2024-02-23 17:01:39 +0200

    adding scripts and configs for RViz and Gazebo

---
+++ rviz.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+rviz2 ./src/config/rviz_config.rviz
--- sim.bash
+++ sim.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 source install/setup.bash
-ros2 launch vox launch_sim.launch.py
+ros2 launch vox launch_sim.launch.py world:=./src/worlds/sim.world
